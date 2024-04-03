869117b9cdcd6b87531fd6fb223ca052cb22054f
Brian Lovell <lovell.brian@gmail.com>
2024-04-03 16:17:04 +0000

    Fairly good version

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -6,8 +6,20 @@
 pip install --upgrade jupyter
 pip install jupyterlab-rise
 pip install fastbook
+sudo apt-get -y update
 sudo apt-get install -y node.js
+pip install -Uqq fastai
+pip install pip_search
+pip install -U ipywidgets==8.0.0
 
-# Commands for presentation
+# Commands for presentationjupyter notebook
 # cd slides
 # jupyter nbconvert birds.ipynb --to slides --post serve
+# Jupyter notebook
+
+# pip uninstall -y jupyter
+# pip uninstall -y jupyterlab
+# pip install jupyter
+# pip install jupyterlab
+# pip install -U jupyterlab_widgets
+# pip install -U ipywidgets
