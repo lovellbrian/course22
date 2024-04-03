c09e5d6a9751657cb5c08ba239429a6142a9e545
Brian Lovell <lovell.brian@gmail.com>
2024-04-03 00:41:47 +0000

    RISE tools added

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -2,3 +2,11 @@
 # pip3 cache purge
 # sudo apt-get autoremove -y
 # sudo apt-get clean
+
+pip install --upgrade jupyter
+pip install jupyterlab-rise
+pip install fastbook
+
+# Commands for presentation
+# cd slides
+# jupyter nbconvert birds.ipynb --to slides --post serve
