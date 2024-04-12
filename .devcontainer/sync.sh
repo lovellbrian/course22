f97a62028d0e6be49fe99be6a4cded6c59cc6642
Brian Lovell <lovell.brian@gmail.com>
2024-04-12 06:37:50 +0000

    Update

--- .devcontainer/sync.sh
+++ .devcontainer/sync.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # This bash shell script allows us the synchronize specified files from the cpufrozen branch to the cpu, gpu, and gpu frozen branches
 
 # Usage: ./sync.sh <file1> <file2> ... <fileN>
