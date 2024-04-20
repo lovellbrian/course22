0934ca07ed8ede7ec01f007d95271dede999cbfb
Brian Lovell <lovell.brian@gmail.com>
2024-04-21 00:04:55 +1000

    Added install-dev-tools.sh

---
+++ .devcontainer/install-dev-tools.sh
@@ -0,0 +1,32 @@
+# Put any custom installs in this file
+
+# Put files in USER_FOLDER. Only do this once. 
+
+echo -e "Creating the /home/vscode/.local python respository\n" 
+
+USER_FOLDER="/home/vscode/.local"
+if [ ! -d "$USER_FOLDER" ]; then
+ 
+  # install python code to ~/.vscode/.local
+  pip install --upgrade pip
+  sudo apt-get install -y libcairo2-dev pkg-config python3-dev
+  pwd
+  pip install -r .devcontainer/requirements.txt
+
+  # Let's have a user version of python3.
+  sudo cp /usr/bin/python3 /home/vscode/.local/bin
+
+  # Version 8 needed for RISE slides. Generates a red compatibility error. 
+  pip install -U ipywidgets==8.0.0
+
+  # Put extra packages here
+
+fi
+
+
+# Notes:
+# Commands for presentation using jupyter notebook
+# cd slides
+# jupyter nbconvert birds.ipynb --to slides --post serve
+# Jupyter notebook
+
