774f16bea193c337b5a3a0cec54c72067412a435
Brian Lovell <lovell.brian@gmail.com>
2024-04-20 12:55:10 +0000

    Added detailed requirements.txt

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -1,14 +1,26 @@
 # Put any custom installs in this file
+
 # pip3 cache purge
 # sudo apt-get autoremove -y
 # sudo apt-get clean
 
+# install python code to ~/.vscode/.local
+pip install --upgrade pip
+sudo apt-get install libcairo2-dev pkg-config python3-dev
+pip install -r requirements.txt
+
+# Let's have a user version of python3.
+cp /usr/bin/python3 /home/vscode/.local/bin
+
+# extra packages
+sudo apt-get -y update
 pip install --upgrade jupyter
 pip install jupyterlab-rise
 pip install fastbook
-sudo apt-get -y update
 pip install -Uqq fastai
 pip install pip_search
+
+# Version 8 needed for RISE slides. Generates a red compatibility error. 
 pip install -U ipywidgets==8.0.0
 
 # Commands for presentationjupyter notebook
@@ -16,9 +28,3 @@ pip install -U ipywidgets==8.0.0
 # jupyter nbconvert birds.ipynb --to slides --post serve
 # Jupyter notebook
 
-# pip uninstall -y jupyter
-# pip uninstall -y jupyterlab
-# pip install jupyter
-# pip install jupyterlab
-# pip install -U jupyterlab_widgets
-# pip install -U ipywidgets
