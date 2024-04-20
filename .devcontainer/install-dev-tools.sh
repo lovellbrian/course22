6566d2d79bfc1fd15af4d094ad6c1e7885c56a68
Brian Lovell <lovell.brian@gmail.com>
2024-04-20 13:10:03 +0000

    Edits

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -1,24 +1,20 @@
 # Put any custom installs in this file
 
-# pip3 cache purge
-# sudo apt-get autoremove -y
-# sudo apt-get clean
-
 # install python code to ~/.vscode/.local
 pip install --upgrade pip
-sudo apt-get install libcairo2-dev pkg-config python3-dev
+sudo apt-get install -y libcairo2-dev pkg-config python3-dev
 pip install -r requirements.txt
 
 # Let's have a user version of python3.
-cp /usr/bin/python3 /home/vscode/.local/bin
+sudo cp /usr/bin/python3 /home/vscode/.local/bin
 
 # extra packages
-sudo apt-get -y update
-pip install --upgrade jupyter
-pip install jupyterlab-rise
-pip install fastbook
-pip install -Uqq fastai
-pip install pip_search
+# sudo apt-get -y update
+# pip install --upgrade jupyter
+# pip install jupyterlab-rise
+# pip install fastbook
+# pip install -Uqq fastai
+# pip install pip_search
 
 # Version 8 needed for RISE slides. Generates a red compatibility error. 
 pip install -U ipywidgets==8.0.0
