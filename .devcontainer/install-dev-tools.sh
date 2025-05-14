e6acb3d5747a4c1cb23cbdc93e7bfe15e75856c7
Brian Lovell <lovell.brian@gmail.com>
2025-05-14 03:43:31 +0000

    Added qtwayland5

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -13,7 +13,7 @@ if [ ! -d "$USER_FOLDER" ]; then
   cp /usr/local/bin/python3.10 /home/vscode/.local/bin
   ln -s /home/vscode/.local/bin/python3.10 /home/vscode/.local/bin/python3
   # Version 8 needed for RISE slides. Generates a red compatibility error. 
-  pip install -U ipywidgets==8.0.0
+  # pip install -U ipywidgets==8.0.0
   # Put extra packages here
 
 else
