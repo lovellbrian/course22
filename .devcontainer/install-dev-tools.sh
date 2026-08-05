df5441f1484c17ed3603bd83ab98279731ecbb37
Brian Lovell <lovell.brian@gmail.com>
2026-08-05 01:38:48 +0000

    Add ipywidgets installation to dev tools script

--- .devcontainer/install-dev-tools.sh
+++ .devcontainer/install-dev-tools.sh
@@ -3,7 +3,7 @@
 # Put any custom installs in this file
 
 # Put files in USER_FOLDER. Only do this once. 
-
+pip install ipywidgets==7.8.0
 # Notes:
 # Commands for presentation using jupyter notebook
 # cd slides
